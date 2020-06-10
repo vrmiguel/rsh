7717cde4a7de80698a6c81a75a45070c5125f91c
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2020-06-10 14:55:20 -0300

    Improves rshexec::simple_command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+/*
+ * rsh
+ * https://github.com/vrmiguel/rsh
+ *
+ * Copyright (c) 2020 Vinícius R. Miguel <vinicius.miguel at unifesp.br>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 mod rshio;
 mod rshexec;
 
@@ -28,8 +53,18 @@ fn main() -> io::Result<()>
         {
             continue;
         }
-        let tokens: Vec<String> = input.split(" ").map(str::to_string).collect();
-        rshexec::run(&tokens, &mut config);
+        let mut tokens: Vec<String> = input.split(" ").map(str::to_string).collect();
+        let mut command: Vec<String> = Vec::new();
+        command.push(tokens[0].clone());
+        tokens.remove(0);
+        command.push(tokens.join(" "));
+
+        if config.is_verbose
+        {
+            println!("bin: {}", command[0]);
+            println!("args: {}", command[1]);
+        }
+        rshexec::run(&command, &mut config);
         if config.exit {
             break;
         }        
--- src/rshexec.rs
+++ src/rshexec.rs
@@ -1,7 +1,30 @@
+/*
+ * rsh
+ * https://github.com/vrmiguel/rsh
+ *
+ * Copyright (c) 2020 Vinícius R. Miguel <vinicius.miguel at unifesp.br>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 use crate::rshio;
 use std::{process::Command, panic};
-
-// TODO: support for commands with tokens.len() > 2
 
 fn simple_command(tokens: &Vec<String>)
 {
@@ -13,7 +36,7 @@ fn simple_command(tokens: &Vec<String>)
 
     let result = panic::catch_unwind(|| 
     {
-        if tokens.len() > 1 
+        if !tokens[1].trim().is_empty()
         {
             let mut child = Command::new(cmd.trim()).arg(&(tokens[1].trim())).spawn().expect("Problem running command.");
             let _ecode = child.wait().expect("Problem waiting for child.");
