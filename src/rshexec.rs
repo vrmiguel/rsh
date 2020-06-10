1d19044344484f33810dedb50920bb3c1430b40c
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2020-06-10 02:16:50 -0300

    Now supports simple commands

--- src/rshexec.rs
+++ src/rshexec.rs
@@ -1,4 +1,22 @@
 use crate::rshio;
+use std::process::Command;
+
+// TODO: support for commands with tokens.len() > 2
+
+fn simple_command(tokens: &Vec<String>)
+{
+    let cmd = String::from("/bin/") + &tokens[0];
+    println!("{:?}", cmd);
+    let err = String::from("Running '") + &tokens[0] + &String::from("' failed.");   // TODO: this is horrible
+    if tokens.len() > 1 {
+        let mut child = Command::new(cmd.trim()).arg(&(tokens[1].trim())).spawn().expect("failed to execute child");
+        let _ecode = child.wait().expect(&err);
+    }
+    else {
+        let mut child = Command::new(cmd.trim()).spawn().expect("failed to execute child");
+        let _ecode = child.wait().expect(&err);
+    }
+}
 
 pub fn run(tokens: &Vec<String>, config: &mut rshio::CLIInput)
 {
@@ -13,5 +31,8 @@ pub fn run(tokens: &Vec<String>, config: &mut rshio::CLIInput)
     {
         config.exit = true;
         return;
+    }
+    else {
+        simple_command(tokens);
     }
 }
