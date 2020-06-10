10409c24466649550b48c241d0102fb51ecffca3
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2020-06-10 12:08:33 -0300

    Supress panic in rshexec::simple_command

--- src/rshexec.rs
+++ src/rshexec.rs
@@ -1,20 +1,32 @@
 use crate::rshio;
-use std::process::Command;
+use std::{process::Command, panic};
 
 // TODO: support for commands with tokens.len() > 2
 
 fn simple_command(tokens: &Vec<String>)
 {
+    panic::set_hook(Box::new(|_info| {
+        // Don't panic. It's all good.
+    }));
+
     let cmd = String::from("/bin/") + &tokens[0];
-    println!("{:?}", cmd);
-    let err = String::from("Running '") + &tokens[0] + &String::from("' failed.");   // TODO: this is horrible
-    if tokens.len() > 1 {
-        let mut child = Command::new(cmd.trim()).arg(&(tokens[1].trim())).spawn().expect("failed to execute child");
-        let _ecode = child.wait().expect(&err);
-    }
-    else {
-        let mut child = Command::new(cmd.trim()).spawn().expect("failed to execute child");
-        let _ecode = child.wait().expect(&err);
+
+    let result = panic::catch_unwind(|| 
+    {
+        if tokens.len() > 1 
+        {
+            let mut child = Command::new(cmd.trim()).arg(&(tokens[1].trim())).spawn().expect("Problem running command.");
+            let _ecode = child.wait().expect("Problem waiting for child.");
+        }
+        else {
+            let mut child = Command::new(cmd.trim()).spawn().expect("Problem running command.");
+            let _ecode = child.wait().expect("Problem waiting for child.");
+        }
+    });
+
+    match result {
+        Ok(res) => res,
+        Err(_) => println!("rsh: problem running command {}", tokens[0].trim()),
     }
 }
 
